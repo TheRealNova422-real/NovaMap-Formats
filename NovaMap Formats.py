031cb070dd431d650b9a05be1768a1408cc7594b
TheRealNova422-real <polaris422@outlook.com>
2024-12-30 06:36:25 +1100

    Update NovaMap Formats.py
    didn't really get much done today, but i did update some of the comments

--- NovaMap Formats.py
+++ NovaMap Formats.py
@@ -1,44 +1,31 @@
 import pathlib, zipfile
 
-# NovaMap Formats, written entirely by Nova422 (The One And Only)
-# Do not pass off, or attempt to pass off any of my code as your own
-#
+# NovaMap File Formats
+
 # NovaMap files are intended to be a quick and easy solution to storing locations
 # of objects and other locations within 2D space.
-#
+
 # NM0 (En-Em-Zero) files are folders using zip compression to house the full map.
-#
+
 # NM1 (En-Em-One) files are positional maps. They contain a set of metadata,
-# intended to be used by the end user to give additional information to the location
-# defined by the file.
-# The metadata begins with the name of the location, followed by a resolution and 
-# scale. Resolution refers to the full size of the map defined by the smallest unit
-# of measurement used to define an object's location.
-# Scale is an optional string which defines the relationship between resolution and
-# size. A scale of 1 meter and resolution of 100x100, as an example, will result
-# in a map of 100 meters by 100 meters.
-# The metadata may also contain other optional details for use by the end user.
-#
-# NM2 (En-Em-Two) files are similar to NM1 files, but are simply objects rather than
-# full locational maps. They may be used interchangably within an NM1, without the
-# requirement of being a map.
-#
-# NM1 files may call other NM1 files, however they may not loop;
-# NM1 file A may call NM1 file B, but if that is so, NM1 file B may not call NM1 file A
-# and will raise an exception. This exception will be ignored if, within the metadata,
-# it contains the tag NonEuclidean = True. 
-# Presently, built in compatibility with non-Euclidean geometry is planned, however
-# I (Nova) have absolutely zero clue how to develop it. Perhaps it will be revealed
-# to me in a dream. Until then, I leave it to you to attempt it, and wish you luck.
-#
+# followed by a list of coordinates.
+
+# NM2 (En-Em-Two) files are references. Similar to NM1s, they contain a set of
+# metadata tags but without any coordinate sets.
+
+# Metadata is stored in the form of Tags, which are accessed by the program and
+# can be accessed at any time.
+
+# NM1 and NM2 files begin with the tags, 
+
 # An example of these files is as follows:
 # MainMap.nm0
-#-> House.nm1       - calls LivingRoom.nm1 and Kitchen.nm1
+# -> House.nm1       - calls LivingRoom.nm1 and Kitchen.nm1
 #   LivingRoom.nm1  - calls Sofa.nm2
 #   Kitchen.nm1     - calls Fridge.nm2
 #   Sofa.nm2        - has "Sittable" tag
 #   Fridge.nm2      - has "Container" tag
-#
+
 
 # Exceptions:
 class MissingMapNM0(Exception):
@@ -51,7 +38,7 @@ class RecursiveNM1(Exception):
 
 # todo: nm1 nm2 plus tags
 
-class nm0file(): # complete
+class nm0file(): # almost complete, just debugging
     def __init__(self, path):
         path = pathlib.Path(path) # pathlib Path object defines what file is opened
         temp = zipfile.ZipFile(path, mode='r') # how the file is read
@@ -134,4 +121,3 @@ bwa = open("mapTest.nm1")
 # print(bwa)
 # bw2 = nm1file(bwa)
 mainfile = nm0file('i.zip')
-
